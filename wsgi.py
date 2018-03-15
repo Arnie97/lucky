6ea6bf46ee54f2643b1ef11e3e34bb0b983bec48
Arnie97 <arnie97@gmail.com>
2018-03-16 03:46:11 +0800

    Build the first version with the Bottle framework.

---
+++ wsgi.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import requests
+from bottle import route, app, abort, HTTPResponse
+
+URL = 'https://www.google.com.hk/search'
+
+
+@route('/<keyword>')
+def feeling_lucky(keyword):
+    params = {'q': keyword, 'hl': 'zh-CN', 'btnI': ''}
+    r = requests.get(URL, params, allow_redirects=False)
+    if r.status_code == 302:
+        return HTTPResponse(r.content, r.status_code, r.headers.items())
+    else:
+        abort(404, 'Not understood.')
+
+
+if __name__ == '__main__':
+    app().run(host='0.0.0.0', port=80, debug=True)
